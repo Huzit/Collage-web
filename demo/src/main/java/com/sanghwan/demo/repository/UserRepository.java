e51c4622579a9ce36a0d2dfcf0e3d104ce98407c
Huzit <hwan4789@gmail.com>
2019-05-17 12:22:55 +0900

    2019-05-15 스승의날 수업

--- demo/src/main/java/com/sanghwan/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sanghwan.demo.repository;
-
-import com.sanghwan.demo.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
